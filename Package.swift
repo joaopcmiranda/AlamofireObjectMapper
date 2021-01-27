d7739cee5b15dd63bc592a9dd94b2de39827fc6a
Joao Miranda <joao@streethawk.co>
2021-01-27 21:14:06 +1100

    Added Swift.package

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AlamofireObjectMapper",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "AlamofireObjectMapper",
+            targets: ["AlamofireObjectMapper"]),
+    ],
+    platforms: [
+        .iOS(.v10),
+        .macOS(.v10_12),
+        .watchOS(.v3),
+        .tvOS(.v10)
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.0.0")),
+        .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", .upToNextMajor(from: "3.5.1"))
+    ],
+    targets: [
+        .target(
+            name: "AlamofireObjectMapper",
+            dependencies: []),
+        .testTarget(
+            name: "AlamofireObjectMapperTests",
+            dependencies: ["AlamoMapper"]),
+    ]
+)
