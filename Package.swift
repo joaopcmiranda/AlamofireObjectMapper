7b38eb6a8be30365d085e18f55677ff5e5c1ca66
Joao Miranda <joao@streethawk.co>
2021-01-27 21:31:56 +1100

    reorganised Package.swift

--- Package.swift
+++ Package.swift
@@ -5,17 +5,17 @@ import PackageDescription
 
 let package = Package(
     name: "AlamofireObjectMapper",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "AlamofireObjectMapper",
-            targets: ["AlamofireObjectMapper"]),
-    ],
     platforms: [
         .iOS(.v10),
         .macOS(.v10_12),
         .watchOS(.v3),
         .tvOS(.v10)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "AlamofireObjectMapper",
+            targets: ["AlamofireObjectMapper"]),
     ],
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.0.0")),
